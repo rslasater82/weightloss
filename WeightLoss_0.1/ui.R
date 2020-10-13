b588095704af115aefc476cbce6ed82013d4db8d
rslasater82 <rslasater82@gmail.com>
2020-10-13 17:43:01 -0400

    Updata 10/13 Multiple buttons added, table will work with plotting values

--- WeightLoss_0.1/ui.R
+++ WeightLoss_0.1/ui.R
@@ -15,15 +15,21 @@ shinyUI(navbarPage("Test ODE output and plotting using Lorenz",
             numericInput("A", "A:", -8/3),
             numericInput("B", "B:", -10),
             numericInput("C", "C:", 28),
-            actionButton("button", "click")
+            numericInput("weight", "Starting Weight:", 200),
+            actionButton("execute", "Calculate")
         ),
-        mainPanel(plotOutput("guessPlot"))
+        mainPanel(plotOutput("guessPlot"),
+                  tableOutput("fullTable"))
     ),
     tabPanel("Table", fluid = TRUE,
          sidebarPanel(
              numericInput("week", "Week: ", 0),
              numericInput("actual", "Actual Weight: ", 200),
-             actionButton("input", "click")
+             fileInput("load", "Load csv", accept = ".csv"),
+             actionButton("userfile", "Use File"),
+             actionButton("addrow", "Add Entry"),
+             actionButton("merge", "Add to Plot"),
+             downloadButton("download", lable = "Export Table")
          ),
          mainPanel(tableOutput("guessTable"))
         )
